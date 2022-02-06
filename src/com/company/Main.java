aaed6296f6f920c9472d74fae322cc5265563fb6
aroslavpetrov <ya.a.petrov@gmail.com>
2022-02-06 15:04:25 +0300

    коммит с задачами повышенной сложности

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -35,5 +35,25 @@ public class Main {
             }
         }
 
+        // Задача 4
+        for (i = 1; i <= 30; i++) {
+            System.out.print(i + ": ");
+            if (i % 3 == 0) {
+                System.out.print("ping ");
+            }
+            if (i % 5 == 0) {
+                System.out.print("pong");
+            }
+            System.out.println();
+        }
+
+        // Задача 5
+        int f0 = 0;
+        int f1 = 1;
+        for (int y = 1; y < 11; y++){
+            System.out.print(f0 + " ");
+            f1 = f0 + f1;
+            f0 = f1 - f0;
+        }
     }
 }
